88edb5aa54bf2ddc81365bb3719d0a1bf025bf40
Bailensn <186718762+Bailensn@users.noreply.github.com>
2026-08-19 16:43:44 +0800

    尝试修复workflow

--- manager/desktop/build.gradle.kts
+++ manager/desktop/build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
     kotlin("jvm") version "2.4.10" apply false
-    id("org.jetbrains.compose") version "1.13.0" apply false
+    id("org.jetbrains.compose") version "1.11.1" apply false
 }
