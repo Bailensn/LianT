65fa8dedccec3cdeb8498b67dd9388e54761fa9d
Bailensn <186718762+Bailensn@users.noreply.github.com>
2026-08-19 16:18:06 +0800

    尝试修复workflow

--- manager/desktop/desktopApp/build.gradle.kts
+++ manager/desktop/desktopApp/build.gradle.kts
@@ -15,27 +15,12 @@ dependencies {
 compose.desktop {
     application {
         mainClass = "com.bailensn.liant.MainKt"
-
         nativeDistributions {
             packageName = "LianT"
             packageVersion = "1.0.0"
-            description = "LianT Manager"
-            vendor = "LianT Team"
-
-            windows {
-                createMsi = false
-                iconFile.set(file("../../resources/windows/LianT.ico"))
-                menu = true
-                perUserInstall = true
-            }
-            macOS {
-                iconFile.set(file("../../resources/darwin/LianT.icns"))
-                bundleID = "com.bailensn.liant"
-                minimumSystemVersion = "11.0"
-            }
-            linux {
-                // 图标 + .desktop 由 CI 脚本动态处理
-            }
+            windows { createMsi = false; iconFile.set(file("../../resources/windows/LianT.ico")) }
+            macOS { iconFile.set(file("../../resources/darwin/LianT.icns")); bundleID = "com.bailensn.liant" }
+            linux { }
         }
     }
 }
